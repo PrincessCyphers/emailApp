791e25ad16a7c6d4c7c7347ee205cbfb6d87c788
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-12 00:07:00 +0000

    main class for email app. to test, creating 2 new email accounts, setting an alt email for 1 and changing the default password for the other

--- src/emailApp.java
+++ src/emailApp.java
@@ -1,2 +1,23 @@
+/* IT support administrator tasked with the creating email accounts for new hires. App should
+- Generate email with firstname.lastname@dept.company.com
+- Determine dept (sales, dev, acct), if none, leave blank
+- Generate random string for password
+- Have method to change password, set mailbox capacity & define alt email address
+- Have methods to display name, email and mailbox capacity
+ */
+
 public class emailApp {
+    public static void main(String[] args) {
+        email em1 = new email("Princess", "Iza");
+
+        email em2 = new email ("Shato", "Kidd");
+        em2.setAlternativeEmail("shatokidd@email.com");
+        System.out.println("Your alternative email is " + em2.getAlternativeEmail());
+
+        System.out.println(em1.showInfo());
+        em1.changePassword("BlueLagoon");
+        System.out.println(em1.getPassword());
+
+
+    }
 }
